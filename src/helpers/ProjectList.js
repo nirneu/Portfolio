f6e96547c7f8c11eac4d4a24c5560a24052dd56b
Nir Neuman <nir.neuman@icloud.com>
2023-12-12 23:05:45 +0200

    Finish basic of all the pages

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,14 @@
+import TagAuto from "../assets/TagAuto.png";
+
+export const ProjectList = [
+  {
+    name: "TagAuto",
+    image: TagAuto,
+    skills: "Swift, SwiftUI, Combine, Async/Await",
+  },
+  {
+    name: "TagAuto",
+    image: TagAuto,
+    skills: "Swift, SwiftUI, Combine, Async/Await",
+  },
+];
