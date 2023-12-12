e40a8d76c8fff65a75efb7429e5baa3706f8c624
Nir Neuman <nir.neuman@icloud.com>
2023-12-12 19:16:10 +0200

    Experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,7 +1,43 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
+import WorkIcon from "@material-ui/icons/Work";
 
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2012 - 2014"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Random school</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Bachelor's Degree
+          </h4>
+          <p>High school diploma</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2014 - 2015"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Random school</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Tel Aviv, Israel
+          </h4>
+          <p>High school diploma</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
