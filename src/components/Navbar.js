94ac19a7b84dec1181c38469e1a5abc74678b69d
Nir Neuman <nir.neuman@icloud.com>
2023-12-13 13:17:58 +0200

    home page with my real info

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,8 +25,8 @@ function Navbar() {
       </div>
       <div className="links">
         <Link to="/">Home</Link>
-        <Link to="/projects">Projects</Link>
         <Link to="/experience">Experience</Link>
+        <Link to="/projects">Projects</Link>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,39 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import EmailIcon from "@material-ui/icons/Email";
 import "../styles/Home.css";
+import ProfilePicture from "../assets/ProfilePicture.png";
 
 function Home() {
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
   return (
     <div className="home">
       <div className="about">
-        <h2>Hi, My Name is Nir</h2>
+        <img src={ProfilePicture} className="profilePicture" />
+        <h2>Nir Neuamn</h2>
         <div className="prompt">
-          <p>An iOS Developer, former Full Stack Developer</p>
-          <LinkedInIcon />
-          <EmailIcon />
-          <GitHubIcon />
+          <p>iOS Developer, Full Stack Developer</p>
+          <a
+            href="https://linkedin.com/in/nirneu"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => openInNewTab("https://linkedin.com/in/nirneu")}
+          >
+            <LinkedInIcon />
+          </a>
+          <a href="mailto:nir.neuman@icloud.com">
+            <EmailIcon />
+          </a>
+          <a
+            href="https://github.com/nirneu"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => openInNewTab("https://github.com/nirneu")}
+          >
+            <GitHubIcon />
+          </a>
         </div>
       </div>
       <div className="skills">
@@ -21,11 +43,23 @@ function Home() {
         <ol className="list">
           <li className="item">
             <h2>iOS Development</h2>
-            <span></span>
+            <span>
+              Swift, SwiftUI, UIKit, Cocoa Touch, Core Data, Combine, MVVM,
+              Xcode
+            </span>
           </li>
           <li className="item">
-            <h2>Full Stack</h2>
-            <span></span>
+            <h2>Full Stack Development</h2>
+            <span>
+              JavaScript, TypeScript, Angular, Node.js, Express, PostgreSQL
+            </span>
+          </li>
+          <li className="item">
+            <h2>Other</h2>
+            <span>
+              Object-Oriented Programming, RESTful APIs, Firebase, Azure Cloud,
+              Git, Scrum, UI/UX
+            </span>
           </li>
           <li className="item">
             <h2>Languages</h2>
